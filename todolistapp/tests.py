e311e40ccc7cc1213793f92a6d6af525ec46e65e
Dmitry <Wherefored@gmail.com>
2021-04-19 00:42:14 +0300

    tests

--- todolistapp/tests.py
+++ todolistapp/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from todolistapp.models import ToDoList
+
+
+class NoteAdderTest(TestCase):
+    def test_adder(self):
+        result = self.client.get('/note-adder/')
+        note = ToDoList.objects.get(position=1)
+        self.assertEqual(result.status_code, 302)
+        self.assertEqual(note.position, 1)
+
+
+class NoteUpdaterTest(TestCase):
+    def test_updater(self):
+        self.client.get('/note-adder/')
+        note = ToDoList.objects.get(position=1)
+        self.assertEqual(note.text, '')
+        self.client.post('/note-updater/1', {'note': 'qwerty'})
+        note = ToDoList.objects.get(position=1)
+        self.assertEqual(note.text, 'qwerty')
+
+
+class NoteDeleterTest(TestCase):
+    def test_deleter(self):
+        self.client.get('/note-adder/')
+        self.client.post('/note-updater/1', {'note': 'qwerty'})
+        note = ToDoList.objects.get(position=1)
+        self.assertEqual(note.text, 'qwerty')
+        self.client.get('/note-deleter/1')
+        notes = ToDoList.objects.filter(position=1)
+        self.assertQuerysetEqual(notes, [])
